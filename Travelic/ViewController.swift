0867da5466de2f15ec184c4e60db82307b7a21c5
fatma95 <fatmataha0@gmail.com>
2018-12-08 01:04:35 +0200

    First commit

---
+++ Travelic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Travelic
+//
+//  Created by Fatma Mohamed on 12/8/18.
+//  Copyright © 2018 Fatma Mohamed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
